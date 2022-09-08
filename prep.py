6c749cdf6d7a8062c1b36248697d16dca867fa88
wmabebe <waqmes@gmail.com>
2022-09-08 14:06:19 -0500

    Fix pickle import

--- prep.py
+++ prep.py
@@ -19,7 +19,7 @@ import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
-import dill as pickle
+import pickle
 
 
 import datetime
